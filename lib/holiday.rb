84751ec75da20a845f7e03df1c97db26588174bc
Damely Tineo <damely.tineo07@gmail.com>
2019-12-22 06:22:38 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -72,7 +72,7 @@ def all_supplies_in_holidays(holiday_hash)
       else 
         print "   " + feast.capitalize + ": "
       end #end if
-print supplies.join(",")
+print supplies.join(", ")
 
       puts ""
     end #end holiday each
