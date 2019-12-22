2eda416235136311c8c427a311e9909bc79dd428
Damely Tineo <damely.tineo07@gmail.com>
2019-12-22 05:43:30 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -61,7 +61,6 @@ def all_supplies_in_holidays(holiday_hash)
     season.capitalize!
     puts "#{season}:"
     holiday.each do |feast, supplies|
-      puts "   #{feast.capitalize}"
       feast = feast.to_s
       if feast.include?("_")
         split_holiday = feast.split("_")
